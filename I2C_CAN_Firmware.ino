b59cec5a6909d9115adf0415e71a898224fe7da3
Tomasz Dobrowolski <chrumck@gmail.com>
2024-02-26 13:07:40 +0100

    refactor to use pro micro

--- I2C_CAN_Firmware.ino
+++ I2C_CAN_Firmware.ino
@@ -1,5 +1,6 @@
-#include <mcp_can.h>
+#include <avr/wdt.h>
 #include <SPI.h>
+#include <mcp2515.h>
 #include <SBWire.h>
 #include <EEPROM.h>
 #include "I2C_CAN_dfs.h"
@@ -11,25 +12,13 @@
 #define CAN_FRAMES_INDEX_SIZE 16    // has to be power of two for hashing to work
 #define CAN_FRAMES_PRUNE_TIME 15000
 #else
-#define CAN_FRAMES_BUFFER_SIZE 12
-#define CAN_FRAMES_INDEX_SIZE 32    // has to be power of two for hashing to work
-#define CAN_FRAMES_PRUNE_TIME 3000
+#define CAN_FRAMES_BUFFER_SIZE 32
+#define CAN_FRAMES_INDEX_SIZE 64    // has to be power of two for hashing to work
+#define CAN_FRAMES_PRUNE_TIME 5000
 #endif
 
-#define CAN_FRAME_SIZE 16
-#define I2C_DATA_LENGTH 20
-#define RECEIVE_REJECTED_RESPONSE 0x01010101
-#define RESPONSE_NOT_READY_RESPONSE 0x01010102
-
-#define SPI_CS_PIN 9            // CAN Bus Shield
-#define LED_PIN 3
+#define CS_PIN 10
 #define SERIAL_BAUD_RATE 115200
-
-#define LEDON()     digitalWrite(LED_PIN, HIGH)
-#define LEDOFF()    digitalWrite(LED_PIN, LOW)
-#define LEDTOGGLE() digitalWrite(LED_PIN, 1 - digitalRead(LED_PIN))
-
-MCP_CAN CAN(SPI_CS_PIN);
 
 CanFrame canFramesBuffer[CAN_FRAMES_BUFFER_SIZE] = { 0 };
 CanFrameIndexEntry canFramesIndex[CAN_FRAMES_INDEX_SIZE] = { 0 };
@@ -59,54 +48,43 @@ u32 getMaskOrFilterValue(u8 regAddress) {
         EEPROM.read(regAddress + 4);
 }
 
+MCP2515 mcp2515(CS_PIN);
+
 void setup()
 {
-    pinMode(LED_PIN, OUTPUT);
-
     Serial.begin(SERIAL_BAUD_RATE);
 
-    for (int i = 0; i < 20; i++)
+    if (EEPROM.read(REG_I2C_ADDRESS_SET) != REG_I2C_ADDRESS_SET_VALUE)
     {
-        LEDTOGGLE();
-        delay(20);
-    }
-
-    int canBaud = EEPROM.read(REG_BAUD);
-    if (canBaud < CAN_5KBPS || canBaud > CAN_1000KBPS) canBaud = CAN_500KBPS;
-
-
-    if (EEPROM.read(REG_ADDR_SET) != 0x5a)
-    {
-        EEPROM.write(REG_ADDR_SET, 0x5a);
-        EEPROM.write(REG_ADDR, DEFAULT_I2C_ADDR);
+        EEPROM.write(REG_I2C_ADDRESS_SET, REG_I2C_ADDRESS_SET_VALUE);
+        EEPROM.write(REG_I2C_ADDRESS, DEFAULT_I2C_ADDRESS);
     }
 
     // I2C setup
-    Wire.begin(EEPROM.read(REG_ADDR));
+    Wire.begin(EEPROM.read(REG_I2C_ADDRESS));
     Wire.onReceive(receiveFromI2C);
     Wire.onRequest(sendToI2C);
 
-    while (CAN.begin(canBaud) != CAN_OK)
-    {
-        delay(100);
-        LEDTOGGLE();
-        Serial.println("CAN FAIL");
-    }
+    int eepromCanBaud = EEPROM.read(REG_CAN_BAUD_RATE);
+    CAN_SPEED canBaud = (eepromCanBaud >= CAN_5KBPS && eepromCanBaud <= CAN_1000KBPS) ? (enum CAN_SPEED)eepromCanBaud : CAN_500KBPS;
+
+    mcp2515.reset();
+    mcp2515.setBitrate(canBaud, MCP_16MHZ);
+    mcp2515.setNormalMode();
 
 #ifndef IS_DEBUG
-    CAN.init_Mask(0, EEPROM.read(REG_MASK0), getMaskOrFilterValue(REG_MASK0));
-    CAN.init_Mask(1, EEPROM.read(REG_MASK1), getMaskOrFilterValue(REG_MASK1));
-    CAN.init_Filt(0, EEPROM.read(REG_FILT0), getMaskOrFilterValue(REG_FILT0));
-    CAN.init_Filt(1, EEPROM.read(REG_FILT1), getMaskOrFilterValue(REG_FILT1));
-    CAN.init_Filt(2, EEPROM.read(REG_FILT2), getMaskOrFilterValue(REG_FILT2));
-    CAN.init_Filt(3, EEPROM.read(REG_FILT3), getMaskOrFilterValue(REG_FILT3));
-    CAN.init_Filt(4, EEPROM.read(REG_FILT4), getMaskOrFilterValue(REG_FILT4));
-    CAN.init_Filt(5, EEPROM.read(REG_FILT5), getMaskOrFilterValue(REG_FILT5));
+    mcp2515.setFilterMask(MCP2515::MASK0, EEPROM.read(REG_MASK0), getMaskOrFilterValue(REG_MASK0));
+    mcp2515.setFilterMask(MCP2515::MASK1, EEPROM.read(REG_MASK1), getMaskOrFilterValue(REG_MASK1));
+    mcp2515.setFilter(MCP2515::RXF0, EEPROM.read(REG_FILT0), getMaskOrFilterValue(REG_FILT0));
+    mcp2515.setFilter(MCP2515::RXF1, EEPROM.read(REG_FILT1), getMaskOrFilterValue(REG_FILT1));
+    mcp2515.setFilter(MCP2515::RXF2, EEPROM.read(REG_FILT2), getMaskOrFilterValue(REG_FILT2));
+    mcp2515.setFilter(MCP2515::RXF3, EEPROM.read(REG_FILT3), getMaskOrFilterValue(REG_FILT3));
+    mcp2515.setFilter(MCP2515::RXF4, EEPROM.read(REG_FILT4), getMaskOrFilterValue(REG_FILT4));
+    mcp2515.setFilter(MCP2515::RXF5, EEPROM.read(REG_FILT5), getMaskOrFilterValue(REG_FILT5));
+
+    wdt_enable(WDTO_1S);
 #endif
 
-    LEDON();
-
-    WD_SET(WD_RST, WDTO_1S);
 }
 
 #define processMaskOrFilterRequest(_register)\
@@ -118,110 +96,124 @@ void setup()
 
 void loop()
 {
-    receiveCanFrame();
+    wdt_reset();
 
-    WDR();
+    receiveCanFrame();
 
     if (i2cReceivedLength < 1 || i2cReceivedLength > I2C_DATA_LENGTH) return;
 
     switch (i2cData[0]) {
 
-    case REG_ADDR: {
+    case REG_I2C_ADDRESS: {
         if (i2cReceivedLength != 2) break;
-        EEPROM.write(REG_ADDR, i2cData[1]);
+        EEPROM.write(REG_I2C_ADDRESS, i2cData[1]);
         while (TRUE);
         break;
     }
 
-    case REG_DNUM: {
-        if (i2cReceivedLength == 1) i2cReadRequest = REG_DNUM;
+    case REG_FRAMES_COUNT: {
+        if (i2cReceivedLength == 1) i2cReadRequest = REG_FRAMES_COUNT;
         break;
     }
 
-    case REG_BAUD: {
-        if (i2cReceivedLength == 1) i2cReadRequest = REG_BAUD;
+    case REG_CAN_BAUD_RATE: {
+        if (i2cReceivedLength == 1) i2cReadRequest = REG_CAN_BAUD_RATE;
         if (i2cReceivedLength != 2) break;
         if (i2cData[1] < CAN_5KBPS || i2cData[1] > CAN_1000KBPS) break;
 
-        while (CAN.begin(i2cData[1]) != CAN_OK) delay(100);
-        EEPROM.write(REG_BAUD, i2cData[1]);
+        CAN_SPEED canBaud = (enum CAN_SPEED)i2cData[1];
+        mcp2515.reset();
+        mcp2515.setBitrate(canBaud, MCP_16MHZ);
+        mcp2515.setListenOnlyMode();
+
+        EEPROM.write(REG_CAN_BAUD_RATE, i2cData[1]);
         break;
     }
 
-    case REG_SEND: {
-        if (i2cReceivedLength != 17) break;
-        if (i2cData[7] > 8) break;
-        if (getCheckSum(i2cData + 1, 15) != i2cData[16]) break;
+    case REG_SEND_FRAME: {
+        if (i2cReceivedLength != I2C_DATA_LENGTH) break;
+        if (i2cData[CAN_FRAME_BIT_DATA_LENGTH + 1] > CAN_DATA_SIZE) break;
+        if (getCheckSum(i2cData + 1, CAN_FRAME_SIZE - 1) != i2cData[CAN_FRAME_BIT_CHECKSUM + 1]) break;
 
-        u32 frameId = i2cData[1] << 24 | i2cData[2] << 16 | i2cData[3] << 8 | i2cData[4];
-        CAN.sendMsgBuf(frameId, i2cData[5], i2cData[7], &i2cData[8]);
+        u32 frameId =
+            i2cData[CAN_FRAME_BIT_ID_0 + 1] << 24 |
+            i2cData[CAN_FRAME_BIT_ID_1 + 1] << 16 |
+            i2cData[CAN_FRAME_BIT_ID_2 + 1] << 8 |
+            i2cData[CAN_FRAME_BIT_ID_3 + 1];
+
+        struct can_frame frame = { .can_id = frameId, .can_dlc = i2cData[CAN_FRAME_BIT_DATA_LENGTH + 1] };
+        memcpy(frame.data, &i2cData[CAN_FRAME_BIT_DATA_0 + 1], CAN_DATA_SIZE);
+        mcp2515.sendMessage(&frame);
         break;
     }
 
-    case REG_RECV: {
+    case REG_RECEIVE_FRAME: {
         if (i2cReceivedLength != 1 && i2cReceivedLength != 5) break;
 
-        i2cReadRequest = REG_RECV;
+        i2cReadRequest = REG_RECEIVE_FRAME;
         memset(&i2cFrameToSend, 0, CAN_FRAME_SIZE);
 
-        u32 frameId = i2cReceivedLength == 5 ? i2cData[1] << 24 | i2cData[2] << 16 | i2cData[3] << 8 | i2cData[4] : NULL;
+        u32 frameId = i2cReceivedLength != 5 ? NULL :
+            i2cData[CAN_FRAME_BIT_ID_0 + 1] << 24 |
+            i2cData[CAN_FRAME_BIT_ID_1 + 1] << 16 |
+            i2cData[CAN_FRAME_BIT_ID_2 + 1] << 8 |
+            i2cData[CAN_FRAME_BIT_ID_3 + 1];
+
         CanFrame* frame = getFrame(frameId);
 
         if (frame == NULL) break;
 
-        i2cFrameToSend[0] = (frame->canId >> 24) & 0xff;
-        i2cFrameToSend[1] = (frame->canId >> 16) & 0xff;
-        i2cFrameToSend[2] = (frame->canId >> 8) & 0xff;
-        i2cFrameToSend[3] = frame->canId & 0xff;
-        i2cFrameToSend[4] = frame->isExtended;
-        i2cFrameToSend[5] = frame->isRemoteRequest;
-        i2cFrameToSend[6] = frame->dataLength;
-        for (int i = 0; i < frame->dataLength; i++) i2cFrameToSend[7 + i] = frame->data[i];
-        i2cFrameToSend[15] = getCheckSum(i2cFrameToSend, 15);
+        i2cFrameToSend[CAN_FRAME_BIT_ID_0] = (frame->canId >> 24) & 0xFF;
+        i2cFrameToSend[CAN_FRAME_BIT_ID_1] = (frame->canId >> 16) & 0xFF;
+        i2cFrameToSend[CAN_FRAME_BIT_ID_2] = (frame->canId >> 8) & 0xFF;
+        i2cFrameToSend[CAN_FRAME_BIT_ID_3] = frame->canId & 0xFF;
+        i2cFrameToSend[CAN_FRAME_BIT_DATA_LENGTH] = frame->dataLength;
+        memcpy(&i2cFrameToSend[CAN_FRAME_BIT_DATA_0], frame->data, frame->dataLength);
+        i2cFrameToSend[CAN_FRAME_BIT_CHECKSUM] = getCheckSum(i2cFrameToSend, CAN_FRAME_SIZE - 1);
 
         break;
     }
 
     case REG_MASK0: {
         processMaskOrFilterRequest(REG_MASK0);
-        CAN.init_Mask(0, i2cData[1], newMaskOrFilter);
+        mcp2515.setFilterMask(MCP2515::MASK0, i2cData[1], newMaskOrFilter);
         break;
     }
 
     case REG_MASK1: {
         processMaskOrFilterRequest(REG_MASK1);
-        CAN.init_Mask(1, i2cData[1], newMaskOrFilter);
+        mcp2515.setFilterMask(MCP2515::MASK1, i2cData[1], newMaskOrFilter);
         break;
     }
 
     case REG_FILT0: {
         processMaskOrFilterRequest(REG_FILT0);
-        CAN.init_Filt(0, i2cData[1], newMaskOrFilter);
+        mcp2515.setFilter(MCP2515::RXF0, i2cData[1], newMaskOrFilter);
         break;
     }
     case REG_FILT1: {
         processMaskOrFilterRequest(REG_FILT1);
-        CAN.init_Filt(1, i2cData[1], newMaskOrFilter);
+        mcp2515.setFilter(MCP2515::RXF1, i2cData[1], newMaskOrFilter);
         break;
     }
     case REG_FILT2: {
         processMaskOrFilterRequest(REG_FILT2);
-        CAN.init_Filt(2, i2cData[1], newMaskOrFilter);
+        mcp2515.setFilter(MCP2515::RXF2, i2cData[1], newMaskOrFilter);
         break;
     }
     case REG_FILT3: {
         processMaskOrFilterRequest(REG_FILT3);
-        CAN.init_Filt(3, i2cData[1], newMaskOrFilter);
+        mcp2515.setFilter(MCP2515::RXF3, i2cData[1], newMaskOrFilter);
         break;
     }
     case REG_FILT4: {
         processMaskOrFilterRequest(REG_FILT4);
-        CAN.init_Filt(4, i2cData[1], newMaskOrFilter);
+        mcp2515.setFilter(MCP2515::RXF4, i2cData[1], newMaskOrFilter);
         break;
     }
     case REG_FILT5: {
         processMaskOrFilterRequest(REG_FILT5);
-        CAN.init_Filt(5, i2cData[1], newMaskOrFilter);
+        mcp2515.setFilter(MCP2515::RXF5, i2cData[1], newMaskOrFilter);
         break;
     }
 
@@ -271,17 +263,17 @@ void sendToI2C() {
 
     switch (i2cReadRequest) {
 
-    case REG_BAUD: {
-        Wire.write(EEPROM.read(REG_BAUD));
+    case REG_CAN_BAUD_RATE: {
+        Wire.write(EEPROM.read(REG_CAN_BAUD_RATE));
         break;
     }
 
-    case REG_DNUM: {
+    case REG_FRAMES_COUNT: {
         Wire.write(canFramesCount);
         break;
     }
 
-    case REG_RECV: {
+    case REG_RECEIVE_FRAME: {
         Wire.write(&i2cFrameToSend[0], CAN_FRAME_SIZE);
         break;
     }
@@ -302,15 +294,25 @@ void sendToI2C() {
 
 void receiveCanFrame()
 {
-    if (CAN.checkReceive() != CAN_MSGAVAIL) return;
+    struct can_frame received = { };
+    MCP2515::ERROR readResult = mcp2515.readMessage(&received);
+
+    if (readResult == MCP2515::ERROR_NOMSG) return;
+
+    if (readResult != MCP2515::ERROR_OK) {
+#ifdef IS_DEBUG
+        Serial.print("Receiving CAN frame failed with error:");
+        Serial.println(readResult);
+#endif
+        return;
+    }
 
     if (canFramesCount == CAN_FRAMES_BUFFER_SIZE) removeOldFrames();
 
     CanFrame frame = { };
-    CAN.readMsgBuf(&frame.dataLength, frame.data);
-    frame.canId = CAN.getCanId();
-    frame.isExtended = CAN.isExtendedFrame();
-    frame.isRemoteRequest = CAN.isRemoteRequest();
+    frame.canId = received.can_id;
+    frame.dataLength = received.can_dlc;
+    memcpy(frame.data, received.data, CAN_DATA_SIZE);
     frame.timestamp = millis();
 
     saveFrame(&frame);
--- I2C_CAN_dfs.h
+++ I2C_CAN_dfs.h
@@ -1,17 +1,36 @@
 #ifndef __I2C_CAN_DFS_H__
 #define __I2C_CAN_DFS_H__
 
-#include <avr/io.h>
+#include <mcp2515.h>
 
 #define FALSE 0
 #define TRUE 1
 #define ULONG_MAX 0xffffffff
-#define CAN_DATA_SIZE 8
+
+#define CAN_DATA_SIZE CAN_MAX_DLEN
+#define CAN_FRAME_SIZE 14
+
+#define CAN_FRAME_BIT_ID_0 0
+#define CAN_FRAME_BIT_ID_1 1
+#define CAN_FRAME_BIT_ID_2 2
+#define CAN_FRAME_BIT_ID_3 3
+#define CAN_FRAME_BIT_DATA_LENGTH 4
+#define CAN_FRAME_BIT_DATA_0 5
+#define CAN_FRAME_BIT_DATA_1 6
+#define CAN_FRAME_BIT_DATA_2 7
+#define CAN_FRAME_BIT_DATA_3 8
+#define CAN_FRAME_BIT_DATA_4 9
+#define CAN_FRAME_BIT_DATA_5 10
+#define CAN_FRAME_BIT_DATA_6 11
+#define CAN_FRAME_BIT_DATA_7 12
+#define CAN_FRAME_BIT_CHECKSUM 13
+
+#define I2C_DATA_LENGTH (CAN_FRAME_SIZE + 1)
+#define RECEIVE_REJECTED_RESPONSE 0x01010101
+#define RESPONSE_NOT_READY_RESPONSE 0x01010102
 
 typedef struct {
   u32 canId;
-  byte isExtended;
-  byte isRemoteRequest;
   byte dataLength;
   byte data[CAN_DATA_SIZE];
   u32 timestamp;
@@ -23,52 +42,23 @@ typedef struct {
   byte bufferPosition;
 } CanFrameIndexEntry;
 
-// watchdog
-#define WD_SET(val,...)                                 \
-    __asm__ __volatile__(                               \
-        "in __tmp_reg__,__SREG__"           "\n\t"      \
-        "cli"                               "\n\t"      \
-        "wdr"                               "\n\t"      \
-        "sts %[wdreg],%[wden]"              "\n\t"      \
-        "sts %[wdreg],%[wdval]"             "\n\t"      \
-        "out __SREG__,__tmp_reg__"          "\n\t"      \
-        :                                               \
-        : [wdreg] "M" (&WDTCSR),                        \
-          [wden]  "r" ((uint8_t)(0x18)),                \
-          [wdval] "r" ((uint8_t)(val|(__VA_ARGS__+0)))  \
-        : "r0"                                          \
-)
+#define DEFAULT_I2C_ADDRESS    0X25
 
-#define WDTO_15MS               0
-#define WDTO_30MS               1
-#define WDTO_60MS               2
-#define WDTO_120MS              3
-#define WDTO_250MS              4
-#define WDTO_500MS              5
-#define WDTO_1S                 6
-#define WDTO_2S                 7
-#define WDTO_4S                 0x20
-#define WDTO_8S                 0x21
+#define REG_I2C_ADDRESS   0X01
+#define REG_FRAMES_COUNT  0x02
+#define REG_CAN_BAUD_RATE 0X03
+#define REG_SEND_FRAME    0X30
+#define REG_RECEIVE_FRAME 0X40
+#define REG_MASK0         0X60
+#define REG_MASK1         0X65
+#define REG_FILT0         0X70
+#define REG_FILT1         0X80
+#define REG_FILT2         0X90
+#define REG_FILT3         0XA0
+#define REG_FILT4         0XB0
+#define REG_FILT5         0XC0
 
-#define WD_RST                  8
-#define WDR()                   __asm__ __volatile__("wdr")
-
-#define DEFAULT_I2C_ADDR    0X25
-
-#define REG_ADDR        0X01
-#define REG_DNUM        0x02
-#define REG_BAUD        0X03
-#define REG_SEND        0X30
-#define REG_RECV        0X40
-#define REG_MASK0       0X60
-#define REG_MASK1       0X65
-#define REG_FILT0       0X70
-#define REG_FILT1       0X80
-#define REG_FILT2       0X90
-#define REG_FILT3       0XA0
-#define REG_FILT4       0XB0
-#define REG_FILT5       0XC0
-
-#define REG_ADDR_SET    0X51
+#define REG_I2C_ADDRESS_SET          0X51
+#define REG_I2C_ADDRESS_SET_VALUE    0x5A
 
 #endif
